99653232451905e1d98e4f7456ee962c6f63dcce
Aidan <abayercalvert@gmail.com>
2023-07-15 16:40:52 -0700

    openapi call setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg'
+import './App.css'
+import triviaQuestions from './data/questions.json'
+
+function App() {
+  return 
+  
+  <div className="App">
+
+
+  </div>
+}
+
+export default App
